266f1d137005c0ed2ed74ac33f853d0717744c58
ianlongden <ianlongden@morgan.harvard.edu>
2019-05-23 15:32:26 -0400

    use constants instead of tuple indexes for easier reading and some flake 7 niceties

--- src/chado_object/chado_base.py
+++ src/chado_object/chado_base.py
@@ -5,13 +5,17 @@
 .. moduleauthor:: Christopher Tabone <ctabone@morgan.harvard.edu>
 """
 import logging
-from harvdev_utils.production import *
-
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.dialects import postgresql
+from harvdev_utils.production import (
+    Cv, Cvterm, Feature, Pub, Synonym
+)
 
 log = logging.getLogger(__name__)
+
+# tuple positions. Allows for clearer reading of code.
+FIELD_NAME = 0
+FIELD_VALUE = 1
+LINE_NUMBER = 3
+
 
 class ChadoObject(object):
     def __init__(self, params):
@@ -32,7 +36,7 @@ class ChadoObject(object):
     def cvterm_query(self, cv_name, cv_term_name, session):
         self.current_query = 'Querying for cv_term_name \'%s\'.' % (cv_term_name)
         log.info(self.current_query)
-        
+
         filters = (
             Cv.name == cv_name,
             Cvterm.name == cv_term_name,
@@ -40,31 +44,31 @@ class ChadoObject(object):
             )
 
         results = session.query(Cv.name, Cvterm.name, Cvterm.is_obsolete, Cvterm.cvterm_id).\
-                join(Cvterm).\
-                filter(*filters).\
-                one()      
+            join(Cvterm).\
+            filter(*filters).\
+            one()
 
         return results[3]
 
     def pub_id_from_fbrf(self, fbrf_tuple, session):
         self.current_query_source = fbrf_tuple
-        self.current_query = 'Querying for FBrf \'%s\'.' % (fbrf_tuple[1])
+        self.current_query = 'Querying for FBrf \'%s\'.' % (fbrf_tuple[FIELD_VALUE])
         log.info(self.current_query)
 
         results = session.query(Pub.uniquename, Pub.pub_id).\
-                filter(Pub.uniquename == fbrf_tuple[1]).\
-                one()
+            filter(Pub.uniquename == fbrf_tuple[FIELD_VALUE]).\
+            one()
 
         return results[1]
 
     def feature_id_from_feature_name(self, feature_name_tuple, session):
         self.current_query_source = feature_name_tuple
-        self.current_query = 'Querying for feature \'%s\'.' % (feature_name_tuple[1])
+        self.current_query = 'Querying for feature \'%s\'.' % (feature_name_tuple[FIELD_VALUE])
         log.info(self.current_query)
 
         results = session.query(Feature.feature_id, Feature.name).\
-                filter(Feature.name == feature_name_tuple[1]).\
-                one()
+            filter(Feature.name == feature_name_tuple[FIELD_VALUE]).\
+            one()
 
         return results[0]
 
@@ -81,12 +85,12 @@ class ChadoObject(object):
 
     def synonym_id_from_synonym_symbol(self, synonym_name_tuple, synonym_type_id, session):
         self.current_query_source = synonym_name_tuple
-        self.current_query = 'Querying for synonym \'%s\'.' % (synonym_name_tuple[1])
+        self.current_query = 'Querying for synonym \'%s\'.' % (synonym_name_tuple[FIELD_VALUE])
         log.info(self.current_query)
 
         results = session.query(Synonym.synonym_id, Synonym.name, Synonym.type_id).\
-                filter(Synonym.name == synonym_name_tuple[1]).\
-                filter(Synonym.type_id == synonym_type_id).\
-                one()
+            filter(Synonym.name == synonym_name_tuple[FIELD_VALUE]).\
+            filter(Synonym.type_id == synonym_type_id).\
+            one()
 
         return results[0]
