3b66bf09167af02e20cfc708040ed06e1cb3eb28
ianlongden <ianlongden@morgan.harvard.edu>
2019-07-02 10:48:25 -0400

    refactored to make flake8 happy and new error parsing actions

--- src/app.py
+++ src/app.py
@@ -7,23 +7,21 @@
 
 # Minimal prototype test for new proforma parsing software.
 # SQL Alchemy imports
-from sqlalchemy import create_engine, event
-from sqlalchemy.orm import relationship, backref, sessionmaker
-from sqlalchemy.dialects import postgresql
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 # TODO Remove unused sqlalchemy modules after finishing transaction scripts.
 
 # System and logging imports
-import os
 import logging
 import sys
 import configparser
 import argparse
 
 # Custom module imports
-from proforma.proforma_operations import *
-from chado_object.conversions.proforma import *
-from transaction.transaction_operations import *
-from error.error_tracking import ErrorTracking
+from proforma.proforma_operations import process_proforma_file, process_proforma_directory
+from chado_object.conversions.proforma import process_data_input
+from transaction.transaction_operations import process_chado_objects_for_transaction
+from error.error_tracking import ErrorTracking, WARNING_ERROR
 
 parser = argparse.ArgumentParser(description='Parse proforma files and load them into Chado.')
 parser.add_argument('-v', '--verbose', help='Enable verbose mode.', action='store_true')
@@ -45,6 +43,7 @@ log = logging.getLogger(__name__)
 config = configparser.ConfigParser()
 config.read(args.config)
 
+
 def create_postgres_session():
     USER = config['connection']['USER']
     PASSWORD = config['connection']['PASSWORD']
@@ -64,8 +63,23 @@ def create_postgres_session():
 
     return session
 
-def obtain_list_of_proformae():
 
+def get_error_summary():
+    """
+    Get the numbers and types or errore generated.
+    Return the counts of critical and warning erros.
+    """
+    critical_count = 0
+    warning_count = 0
+    for instance in ErrorTracking.instances:
+        if instance.error_level == WARNING_ERROR:
+            warning_count += 1
+        else:
+            critical_count += 1
+    return critical_count, warning_count
+
+
+def obtain_list_of_proformae():
     # Obtain file list of proformae to be processed.
     directory_to_process = args.directory
 
@@ -73,67 +87,57 @@ def obtain_list_of_proformae():
 
     return list_of_proformae
 
-def main(session, list_of_proformae):
 
-    curator_dict = dict(config['curators'])
-
-    dict_of_processed_files = dict()
-
-    log.info('Opening and processing the list of proformae.')
-
-    for proforma_location in list_of_proformae:
-        list_of_processed_proforma_objects = process_proforma_file(proforma_location, curator_dict) # Processing individual proforma file.
-
-        for processed_proforma_object in list_of_processed_proforma_objects:
-            filename = processed_proforma_object.get_file_metadata().get('filename')
-
-            if filename not in dict_of_processed_files:
-                    dict_of_processed_files[filename] = [] # Create a list within the dictionary entry if we don't have it already.
-            dict_of_processed_files[filename].append(processed_proforma_object) # Add the processed proforma object as a dict entry under the filename.
-
-    log.info('--------------------')
-    log.info('')
-    log.info('Validation Summary.')
-    log.info('Processed %s file(s).' % (len(dict_of_processed_files)))
-
-    list_of_errors_validation = [instance for instance in ErrorTracking.instances]
-
-    if len(list_of_errors_validation) > 0:
-        for error_object in list_of_errors_validation:
-            error_object.print_error_messages()
-
-    if len(list_of_errors_validation) > 0:
-        log.critical('At least one errored file(s) found.')
-        log.critical('Please fix or remove these file(s) before proceeding.')
-        log.critical('No data was loaded into Chado.')
-        log.critical('Exiting.')
-        sys.exit(-1)
-    else:
-        log.info('All files successfully validated! Proceeding to transactions.')
-        log.info('')
-
-    main_list_of_chado_objects_to_load = []
-
-    for filename in dict_of_processed_files:
-        log.info('Converting proforma from file %s into ChadoObjects' % (filename))
-        for proforma_object_to_load in dict_of_processed_files[filename]:
-            returned_list_of_chado_objects = process_data_input(proforma_object_to_load)
-            main_list_of_chado_objects_to_load.extend(returned_list_of_chado_objects)
-
-    load_type = args.load_type
+def check_load_type(load_type):
+    """
+    Exit immediately if load type is not one of the recognised types.
+    """
     if load_type not in ('test', 'production'):
         log.critical('load_type must be specified as either \'test\' or \'production\'')
         log.critical('Exiting.')
         sys.exit(-1)
 
-    # Send our list of Chado Objects off to be loaded into the database.
-    process_chado_objects_for_transaction(session, main_list_of_chado_objects_to_load, load_type)
+
+def process_proforma(list_of_proformae):
+    """
+    Process the list of proforma and return a dict of object
+    """
+    dict_of_processed_files = dict()
+    curator_dict = dict(config['curators'])
+    for proforma_location in list_of_proformae:
+        list_of_processed_proforma_objects = process_proforma_file(proforma_location, curator_dict)  # Processing individual proforma file.
+
+        for processed_proforma_object in list_of_processed_proforma_objects:
+            filename = processed_proforma_object.get_file_metadata().get('filename')
+
+            if filename not in dict_of_processed_files:
+                dict_of_processed_files[filename] = []  # Create a list within the dictionary entry if we don't have it already.
+            dict_of_processed_files[filename].append(processed_proforma_object)  # Add the processed proforma object as a dict entry under the filename.
 
     log.info('--------------------')
     log.info('')
-    log.info('Transactions complete.')
-    log.info('')
+    log.info('Validation Summary.')
+    log.info('Processed %s file(s).' % (len(dict_of_processed_files)))
+    return dict_of_processed_files
 
+
+def proforma_to_chado(dict_of_processed_files):
+    """
+    Convert the proforma object to chado ones.
+    """
+    main_list_of_chado_objects_to_load = []
+    for filename in dict_of_processed_files:
+        log.info('Converting proforma from file %s into ChadoObjects' % (filename))
+        for proforma_object_to_load in dict_of_processed_files[filename]:
+            returned_list_of_chado_objects = process_data_input(proforma_object_to_load)
+            main_list_of_chado_objects_to_load.extend(returned_list_of_chado_objects)
+    return main_list_of_chado_objects_to_load
+
+
+def process_errors(load_type):
+    """
+    Look at the errors and generate a summary and exit without savng if any critical errors
+    """
     list_of_errors_transactions = [instance for instance in ErrorTracking.instances]
 
     if len(list_of_errors_transactions) > 0:
@@ -141,11 +145,17 @@ def main(session, list_of_proformae):
             error_object.print_error_messages()
 
     if len(list_of_errors_transactions) > 0:
-        log.critical('At least one errored file(s) found.')
-        log.critical('Please fix or remove these file(s) before proceeding.')
-        log.critical('No data was loaded into Chado.')
-        log.critical('Exiting.')
-        sys.exit(-1)
+        critical_count, warning_count = get_error_summary()
+        if critical_count:
+            log.critical('{} critical errors were found.'.format(critical_count))
+            log.critical('{} warning errors were found.'.format(warning_count))
+            log.critical('Please the correct at least the critical ones or remove these file(s) before proceeding.')
+            log.critical('No data was loaded into Chado.')
+            log.critical('Exiting.')
+            sys.exit(-1)
+        elif warning_count:
+            log.warning("{} warnings found. Do you wish to proceed WILL be added soon".format(warning_count))
+            log.warning("Ignoring warnings for now!!!!")
     else:
         if load_type == 'test':
             log.info('All files successfully tested against production Chado.')
@@ -154,6 +164,30 @@ def main(session, list_of_proformae):
             log.info('All files successfully loaded into Chado.')
             log.info('bingo ....you success !....')
             log.info('')
+
+
+def main(session, list_of_proformae):
+
+    check_load_type(args.load_type)
+
+    log.info('Opening and processing the list of proformae.')
+    dict_of_processed_files = process_proforma(list_of_proformae)
+
+    log.info("Starting conversion of proforma object to chado.")
+    main_list_of_chado_objects_to_load = proforma_to_chado(dict_of_processed_files)
+
+    # Send our list of Chado Objects off to be loaded into the database.
+    log.info("Starting to load chado object into the database.")
+    process_chado_objects_for_transaction(session, main_list_of_chado_objects_to_load, args.load_type)
+
+    log.info('--------------------')
+    log.info('')
+    log.info('Transactions complete.')
+    log.info('')
+
+    # List errors and warning and Exit if any are critical
+    process_errors(args.load_type)
+
 
 if __name__ == '__main__':
     session = create_postgres_session()
