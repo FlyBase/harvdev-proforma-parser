40ebdce28e3a608063ea61cab8fde5b91f0a9f20
ianlongden <ianlongden@morgan.harvard.edu>
2019-11-18 11:09:01 -0500

    Give pumchempy a few chances to connect etc as it can time out quite esaily and stops the tests

--- src/chado_object/chado_chem.py
+++ src/chado_object/chado_chem.py
@@ -443,11 +443,30 @@ class ChadoChem(ChadoObject):
         if not self.chemical_information['description']['data']:  # If the list is empty.
 
             log.info('Searching for definitions on PubChem.')
-            results = pubchempy.get_compounds(identifier_name, 'name')
+            results = None
+            count = 0
+            while not results and count < 5:
+                try:
+                    results = pubchempy.get_compounds(identifier_name, 'name')
+                except pubchempy.PubChemHTTPError:
+                    log.info('Chem server busy or timedout, waiting to try again')
+                    count += 1
+                    wait(5)
+            # should we check the results here?
+
             cid_for_definition = results[0].cid
             log.info('Found PubChem CID: {} from query using {}'. format(cid_for_definition, identifier_name))
-
-            description = pubchempy.request(cid_for_definition, operation='description')
+            count = 0
+            description = None
+            # pubchemby can be a bit flaky so try 5 times with a small break inbetween
+            while not description and count < 5:
+                try:
+                    description = pubchempy.request(cid_for_definition, operation='description')
+                except pubchempy.PubChemHTTPError:
+                    log.info('Chem server busy or timedout, waiting to try again')
+                    count += 1
+                    wait(5)
+            # Check for description ?
 
             raw_data = description.read()
             encoding = description.info().get_content_charset('utf8')  # JSON default
