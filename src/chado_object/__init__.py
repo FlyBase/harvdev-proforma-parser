fe55eaac5a5454c7f8b7392914bd611d4ec60c40
ianlongden <ianlongden@morgan.harvard.edu>
2019-12-17 14:23:24 -0500

    first species draft

--- src/chado_object/__init__.py
+++ src/chado_object/__init__.py
@@ -3,4 +3,5 @@ from .chado_gene import ChadoGene
 from .chado_pub import ChadoPub
 from .chado_chem import ChadoChem
 from .humanhealth.chado_humanhealth import ChadoHumanhealth
+from .chado_species import ChadoSpecies
 from .chado_exceptions import ValidationError
