c86af3d24d74072ffe924b0f2112f12d9a7eaa86
ianlongden <ianlongden@morgan.harvard.edu>
2023-09-27 10:50:53 -0400

    new geneproducts and F2 done.

--- src/chado_object/geneproduct/chado_geneproduct.py
+++ src/chado_object/geneproduct/chado_geneproduct.py
@@ -9,10 +9,12 @@ import re
 from chado_object.chado_base import FIELD_VALUE
 from chado_object.feature.chado_feature import ChadoFeatureObject
 from harvdev_utils.production import (
-    Feature, FeaturePub, Featureprop, FeatureCvterm, FeatureCvtermprop,
-    Organism, Cvterm, Cv, Synonym, Db, Dbxref, Pub
+    Feature, FeaturePub, FeatureRelationshipPub, FeatureRelationship,
+    FeatureCvterm
+    # Featureprop, FeatureCvtermprop,
+    # Organism, Cvterm, Cv, Synonym, Db, Dbxref, Pub
 )
-from harvdev_utils.chado_functions import get_or_create, get_cvterm
+from harvdev_utils.chado_functions import get_or_create, get_cvterm, feature_name_lookup
 from harvdev_utils.chado_functions.organism import get_organism
 # from error.error_tracking import CRITICAL_ERROR
 import logging
@@ -45,7 +47,7 @@ class ChadoGeneProduct(ChadoFeatureObject):
         # This is set in the Feature.yml file.
         ##########################################
         self.type_dict = {#'prop': self.load_prop,
-                          # 'cvterm': self.load_cvterm,
+                          'cvterm': self.load_cvterm,
                           'synonym': self.load_synonym,
                           #'dissociate_pub': self.dissociate_pub,
                           #'obsolete': self.make_obsolete,
@@ -55,9 +57,9 @@ class ChadoGeneProduct(ChadoFeatureObject):
         }
 
         self.delete_dict = {#'dbxrefprop': self.delete_dbxref,
-                            'ignore': self.delete_ignore
+                            'ignore': self.delete_ignore,
                             #'prop': self.delete_prop,
-                            #'cvterm': self.delete_cvterm,
+                            'cvterm': self.delete_cvterm
                             #'synonym': self.delete_synonym,
                             #'featureprop': self.delete_featureprop,
                             #'relationship': self.delete_relationship
@@ -117,106 +119,198 @@ class ChadoGeneProduct(ChadoFeatureObject):
         log.debug('Curator string assembled as:')
         log.debug(curated_by_string)
 
-    def get_uniquename_and_checks(self):
-        # Need to return string based on F3 content.
+    def load_cvterm(self, key):
+        if self.has_data(key):
+            cvterm = get_cvterm(self.session, self.process_data[key]['cv'], self.process_data[key]['cvterm'])
+            if not cvterm:
+                message = 'Cvterm lookup failed for cv {} cvterm {}?'.format(self.process_data[key]['cv'],
+                                                                             self.process_data[key]['cvterm'])
+                self.critical_error(self.process_data[key]['data'], message)
+                return
+            get_or_create(self.session, FeatureCvterm,
+                          feature_id=self.feature.feature_id,
+                          cvterm_id=cvterm.cvterm_id,
+                          pub_id=self.pub.pub_id)
 
-        name = self.process_data['F1a']['data'][FIELD_VALUE]
+    def delete_cvterm(self, key, bangc):
+        pass
 
+    def check_format(self, status):
         format_okay = False
+
+        name = status['name']
         if name.endswith('-XP') or name.endswith('-XR'):
             if self.has_data('F2'):
                 message = f"F1a {name} cannot end in -XR or -XP if F2 is defined"
                 self.critical_error(self.process_data['F1a']['data'], message)
-                return
-            format_okay = True
+            else:
+                format_okay = True
         elif name.endswith(']PA') or name.endswith(']RA'):
             if not self.has_data('F2'):
                 message = f"Require F2 value if {name} is transgenic (not -XR or -XP)"
                 self.critical_error(self.process_data['F1a']['data'], message)
-            format_okay = True
+            else:
+                format_okay = True
         elif '&cap;' in name:
             if not self.has_data('F2'):
                 message = f"Require F2 value if {name} is transgenic (not -XR or -XP)"
                 self.critical_error(self.process_data['F1a']['data'], message)
-            format_okay = True
+            else:
+                format_okay = True
         if not format_okay:
             message = f"F1a {name} must end with -XR, -XP, ]PA, ]RA, or, contain '&cap;' for a split system combination feature"
             self.critical_error(self.process_data['F1a']['data'], message)
-            return
+            status["error"] = True
 
+    def check_type(self, status):
         pattern = r'(.*) (\w+):(\d+)'
         s_res = re.search(pattern, self.process_data['F3']['data'][FIELD_VALUE])
-        type_name = ""
-        feat_type = None
         if s_res:
             type_name = s_res.group(1)
             cv_name = s_res.group(2)
-            # end_name = s_res.group(3)
-            feat_type = get_cvterm(self.session, cv_name, type_name)
+            if cv_name == 'FBcv':
+                cv_name = 'FlyBase miscellaneous CV'
+            status["feat_type"] = get_cvterm(self.session, cv_name, type_name)
+            status["type_name"] = type_name
         else:
-            message = "Does not fit format, expected the format (.*) (\w+):(\d+)"
+            message = r"Does not fit format, expected the format (.*) (\w+):(\d+)"
             self.critical_error(self.process_data['F3']['data'], message)
+            status["error"] = True
             return
 
         if type_name == 'protein':
             message = "Type can not be 'protein', should be 'polypeptide"
             self.critical_error(self.process_data['F3']['data'], message)
-            return
+            status["error"] = True
         if type_name != 'split system combination':
-            if 'INTERSECTION' in name or '&cap;' in name:
-                message = f"split system combination feature {name} must have type 'split system combination FBcv:0009026' in F3."
+            if 'INTERSECTION' in status['name'] or '&cap;' in status['name']:
+                message = f"split system combination feature {status['name']} must have type 'split system combination FBcv:0009026' in F3."
                 self.critical_error(self.process_data['F3']['data'], message)
-                return
+                status["error"] = True
 
-        fb_prefix = "FB"
+    def check_type_name(self, status):
+        if 'type_name' not in status:
+            return
+
+        name = status['name']
+
+        type_name = status['type_name']
         if type_name == 'split system combination':
-            fb_prefix += "co"
+            status['fb_prefix'] = "FBco"
             if '&cap;' not in name:
                 message = f"new split system combination feature {name} must have '&cap;' in its name"
                 self.critical_error(self.process_data['F1a']['data'], message)
-                return
+                status["error"] = True
 
             pattern = "(XR|XP|R[A-Z]|P[A-Z])$"
             s_res = re.findall(pattern, name)
             if s_res:
                 message = f"new split system combination feature {name} must not have any XR/XP/RA/PA suffix in its name"
                 self.critical_error(self.process_data['F1a']['data'], message)
-                return
+                status["error"] = True
 
             pattern = "DBD.*AD"
             s_res = re.findall(pattern, name)
             if not s_res:
                 message = f"new split system combination feature {name} must list DBD before AD;"
                 self.critical_error(self.process_data['F1a']['data'], message)
-                return
+                status["error"] = True
+
         elif type_name == 'polypeptide':
-            fb_prefix += "pp"
+            status['fb_prefix'] = "FBpp"
             pattern = "(-XP|P[A-Z])$"
             s_res = re.findall(pattern, name)
             if not s_res:
                 message = f"polypeptide {name} should be ended with -XP or PA"
                 self.critical_error(self.process_data['F1a']['data'], message)
-                return
+                status["error"] = True
         elif type_name.endswith('RNA'):
-            fb_prefix += "tr"
+            status['fb_prefix'] = "FBtr"
             pattern = "(-XR|R[A-Z])$"
             s_res = re.findall(pattern, name)
             if not s_res:
                 message = f"transcript {name} should be ended with -XR or RA"
                 self.critical_error(self.process_data['F1a']['data'], message)
-                return
+                status["error"] = True
         else:
             message = f"unexpected F3 value: {type_name}"
             self.critical_error(self.process_data['F3']['data'], message)
-            return
-        return fb_prefix, feat_type
+            status["error"] = True
 
-    def get_org(self):
-        pattern = r'^(.{2,14}?)\\'
-        s_res = re.search(pattern, self.process_data['F1a']['data'][FIELD_VALUE])
+    def get_feats(self, status):
+        """Lookup the features from the base name/s"""
+        status['features'] = []
+        feats = []
+        if status['type_name'] == 'split system combination':
+            feats = status['name'].split('&cap;')
+        else:
+            pattern = '^(.*)(-X[RP]|[RP][A-Z])$'
+            res = re.match(pattern, status['name'])
+            if res:
+                feats.append(res.group(1))
+            else:
+                message = f"Could not find existing feature in the name."
+                self.critical_error(self.process_data['F1a']['data'], message)
+                status["error"] = True
+                return
+
+        for feat in feats:
+            feat = feature_name_lookup(self.session, name=feat, obsolete='f')
+            if feat:
+                log.debug(f"BOB: Feature lookup found: {feat}")
+                status['features'].append(feat.feature_id)
+            else:
+                message = f"Could not find feature {feat} in the database to add the feature relationship"
+                self.critical_error(self.process_data['F1a']['data'], message)
+                status["error"] = True
+
+    def get_uniquename_and_checks(self):
+        # Need to return string based on F3 content.
+        # return dict of :-
+        #
+        # error: [True/False]
+        # features : [features producing this new product]  # NOTE 2 of these for splits
+        # fb_prefix: string (FBxx)
+        # feat_type: cvterm object
+
+        status = {'error': False,
+                  'name': self.process_data['F1a']['data'][FIELD_VALUE]}
+        self.check_format(status)
+        self.check_type(status)
+        self.check_type_name(status)
+        self.get_feats(status)
+
+        return status
+
+    def add_relationships(self, status):
+
+        cvterm_name = 'associated_with'
+        cv_name = 'relationship type'
+        if status['type_name'] == 'split system combination':
+            cvterm_name = 'partially_produced_by'
+        cvterm = get_cvterm(self.session, cv_name=cv_name, cvterm_name=cvterm_name)
+
+        pub_id = self.get_unattrib_pub().pub_id
+        for feat_id in status['features']:
+            fr, _ = get_or_create(self.session, FeatureRelationship,
+                                  subject_id=self.feature.feature_id,
+                                  object_id=feat_id,
+                                  type_id=cvterm.cvterm_id)
+
+            frp, _ = get_or_create(self.session, FeatureRelationshipPub,
+                                   feature_relationship_id=fr.feature_relationship_id,
+                                   pub_id=pub_id)
+
+    def get_org(self, status):
         abbr = 'Dmel'
-        if s_res:
-            abbr = s_res[1]
+
+        if status['type_name'] == 'split system combination':
+            abbr = 'Ssss'
+        else:
+            pattern = r'^(.{2,14}?)\\'
+            s_res = re.search(pattern, self.process_data['F1a']['data'][FIELD_VALUE])
+            if s_res:
+                abbr = s_res[1]
         org = get_organism(self.session, short=abbr)
         return org
 
@@ -224,18 +318,22 @@ class ChadoGeneProduct(ChadoFeatureObject):
         if self.new:
 
             # get type/uniquename from F3.
-            uniquename_prefix, feat_type = self.get_uniquename_and_checks()
-            organism = self.get_org()
+            status = self.get_uniquename_and_checks()
+            if status['error']:
+                return
+            organism = self.get_org(status)
             gp, _ = get_or_create(self.session, Feature, name=self.process_data['F1a']['data'][FIELD_VALUE],
-                                  organism_id=organism.organism_id, uniquename=f'{uniquename_prefix}:temp_0',
-                                  type_id=feat_type.cvterm_id)
+                                  organism_id=organism.organism_id, uniquename=f'{status["fb_prefix"]}:temp_0',
+                                  type_id=status['feat_type'].cvterm_id)
 
             # db has correct FBhh0000000x in it but here still has 'FBhh:temp_0'. ???
             # presume triggers start after hh is returned. Maybe worth getting form db again
-            log.debug(f"New expression created {gp.uniquename} id={gp.feature_id}.")
+            log.debug(f"New gene product created {gp.uniquename} id={gp.feature_id}.")
+
             self.feature = gp
-            self.load_synonym('HH1b', cvterm_name='symbol')  # add symbol
-            self.load_synonym('HH1b')                       # add fullname
+            self.add_relationships(status)
+            self.load_synonym('F1a')  # add symbol
+            # self.load_synonym('F1a')                       # add fullname HAS NONE
         else:
             not_obsolete = False
             gp = self.session.query(Feature).\
